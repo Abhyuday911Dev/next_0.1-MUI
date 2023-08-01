bc9cc8a8f717d8de50f7446a3adf9586e9f21f9c
Abhyuday <abhyuday911@gmail.com>
2023-08-01 18:25:53 +0530

    cleanup

--- app/page.js
+++ app/page.js
@@ -6,20 +6,9 @@ const page = () => {
   const [name, setName] = useState("john doe");
   const [time, setTime] = useState(null);
 
-  const clickHandler = () => {
-    name === "john doe" ? setName("not john doe") : setName("john doe");
-  };
-
-  setInterval(() => {
-    setTime(new Date().toLocaleTimeString())
-  },1000)
-
   return (
     <div>
-      Hello World!
-      <h1>{name} {time}</h1>
-      <button onClick={() => clickHandler()}>Change name</button>
-      {/* <p>{time}</p> */}
+      <form></form>
     </div>
   );
 };
