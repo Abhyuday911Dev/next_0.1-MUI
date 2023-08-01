7f3b4fdc6b21d3de1b84edbc879127564d8b09a0
Abhyuday <abhyuday911@gmail.com>
2023-08-01 18:32:04 +0530

    Form

--- app/page.js
+++ app/page.js
@@ -6,9 +6,19 @@ const page = () => {
   const [name, setName] = useState("john doe");
   const [time, setTime] = useState(null);
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const { username } = event.target;
+    setName(username.value);
+  };
+
   return (
     <div>
-      <form></form>
+      <h1>Name: {name}</h1>
+      <form onSubmit={submitHandler}>
+        <input type="text" name="username" />
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
